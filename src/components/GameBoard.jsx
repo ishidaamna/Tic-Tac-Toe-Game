235f0962451e5774c923e570ce3395be30bab5be
Ishida Amuna <134792769+ishidaamna@users.noreply.github.com>
2025-04-21 21:16:43 +0900

    Removed the comment from the code.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,19 +1,5 @@
 // The main gameboard function
 export default function GameBoard({ onSelectSquare, board }) {
-  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // This is a function that runs when someone clicks a square.
-  // It knows which row and column got clicked (like row 1, column 2).
-  //   function handleSelectSquare(rowIndex, colIndex) {
-  //     setGameBoard((prevGameBoard) => {
-  // Before changing the board, we make a copy. Why? Because React likes when we don’t touch the old board directly.
-  //       const updatedBoard = prevGameBoard.map((row) => [...row]);
-  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //       return updatedBoard;
-  //     });
-  //     onSelectSquare();
-  //   }
-
   return (
     <ol id="game-board">
       {/* means “go through each row of the board.” */}
